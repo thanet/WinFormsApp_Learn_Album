0d7fb69f1672e0bc2db3a182b46499b13f588210
Thanet_y <thanet_y@yahoo.com>
2025-11-20 15:18:51 +0700

    update

--- WinFormsApp_Learn/AlbumsDAO.cs
+++ WinFormsApp_Learn/AlbumsDAO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -7,6 +8,27 @@ namespace WinFormsApp_Learn
     internal class AlbumsDAO
     {
         // version 1 only contains fake data. No connection to actual db
-        public List<Album> albums = new List<Album>() ;
+        //public List<Album> albums = new List<Album>() ;
+
+        // connect to Mysql database
+
+        string connectString = "datasource=localhost;port=3306;username=root;password=312314," +
+            "                   database=music;";
+
+        public List<Album> getAllAlbums()
+        {
+            // start with an empty list
+            List<Album> returnThese = new List<Album>();
+
+            // connect to the mysql server
+            MySqlConnection connection = new MySqlConnection
+                (connectString);
+            connection.Open();
+
+            return returnThese;
+
+        }
+
+
     }
 }
--- WinFormsApp_Learn/Form1.cs
+++ WinFormsApp_Learn/Form1.cs
@@ -16,30 +16,30 @@ namespace WinFormsApp_Learn
             lblHellowWorld.Text = "Hello World";
 
             AlbumsDAO albumsDAO = new AlbumsDAO();
-            Album a1 = new Album
-            {
-                ID = 1,
-                AlbumName = "My first album",
-                ArtistName = "Thanet Yan",
-                Year = 1975,
-                ImageURL = "Not Yet",
-                Description = "Nothing Special"
-            };
-            Album a2 = new Album
-            {
-                ID = 2,
-                AlbumName = "My second album",
-                ArtistName = "Thanet Yan",
-                Year = 1976,
-                ImageURL = "Not Yet",
-                Description = "Nothing Special"
-            };
+            //Album a1 = new Album
+            //{
+            //    ID = 1,
+            //    AlbumName = "My first album",
+            //    ArtistName = "Thanet Yan",
+            //    Year = 1975,
+            //    ImageURL = "Not Yet",
+            //    Description = "Nothing Special"
+            //};
+            //Album a2 = new Album
+            //{
+            //    ID = 2,
+            //    AlbumName = "My second album",
+            //    ArtistName = "Thanet Yan",
+            //    Year = 1976,
+            //    ImageURL = "Not Yet",
+            //    Description = "Nothing Special"
+            //};
 
-            albumsDAO.albums.Add(a1);
-            albumsDAO.albums.Add (a2);
+            //albumsDAO.albums.Add(a1);
+            //albumsDAO.albums.Add (a2);
 
             // connect the list to the grid view control
-            albumBindingSource.DataSource = albumsDAO.albums;
+            albumBindingSource.DataSource = albumsDAO.getAllAlbums;
 
             dgv_Album.DataSource = albumBindingSource;  // we can use   albumsDAO.albums;
 
